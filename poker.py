dc562bca1bfac35aafabeb26989497dbfbfc5a2f
davidoxford <david@oakhaven.org>
2019-02-08 12:20:58 -0500

    Added checkPairings function

--- poker.py
+++ poker.py
@@ -14,27 +14,36 @@ def cardval(cardnum):
     return pval
 #-------------------------------------------------------------------
 def isFlush(hand):
+    first_card = hand[0]        #Look at the first card
+    first_card_rank = first_card[0]
+
     suits = [card[1] for card in hand]
     suitset = set(suits)    #sets remove duplicates
     if len(suitset) == 1:
-        return True
+        return first_card_rank
     else:
         return False
 
 #-------------------------------------------------------------------
 def isStraight(hand):
+    first_card = hand[0]        #Look at the first card
+    first_card_rank = first_card[0]
+
     ranks = [card[0] for card in hand]  #Hand is assumed to be sorted in desc order
 
     for x in range(len(ranks)-1):    #Loop through the first four cards, looking at it and the next card
         if ranks[x+1] != ranks[x]-1: #If the next card isn't one less than this card, it ain't a straight
             return False
 
-    return True
+    return first_card_rank
 
 #-------------------------------------------------------------------
 def isStraightFlush(hand):
+    first_card = hand[0]        #Look at the first card
+    first_card_rank = first_card[0]
+
     if isStraight(hand) and isFlush(hand):
-        return True
+        return first_card_rank
     else:
         return False
 
@@ -45,9 +54,23 @@ def isRoyalFlush(hand):
         return False
 
     if isStraight(hand) and isFlush(hand) :
-        return True
+        return 14
     else:
         return False
+
+#-------------------------------------------------------------------
+def checkPairings(hand):
+    ranks = [card[0] for card in hand]  #Get list of ranks of current hand
+
+    rank_counts = {ranks[0] : 1}  #Count the first card
+    for x in range(1, len(ranks)-1):
+        cur_rank = ranks[x]
+        if cur_rank in rank_counts.keys():
+            rank_counts[cur_rank] = rank_counts[cur_rank] + 1
+        else:
+            rank_counts[cur_rank] = 1
+
+    return rank_counts
 
 #-------------------------------------------------------------------
 
@@ -94,3 +117,6 @@ print ('Straight Flush?', straight)
 
 straight = isRoyalFlush(ha1)
 print ('Royal Flush?', straight)
+
+pairings = checkPairings(ha1)
+print ('Pairings:', pairings)
