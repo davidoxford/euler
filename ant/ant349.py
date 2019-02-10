8a1ae2439c918c0216c9ea6ed8ce39234e31f21a
davidoxford <david@oakhaven.org>
2019-02-10 11:21:36 -0500

    Adjusted starting parameters and added progress indicator and end-of-game stats. Also allowed for graceful exit when exceeding the grid boundary.

--- ant/ant349.py
+++ ant/ant349.py
@@ -1,44 +1,121 @@
-def move(xpos, ypos, direction):
-    next_x = xpos
-    next_y = ypos
-    if direction == 1:
-        next_y -= 1
-    elif direction == 2:
-        next_x += 1
-    elif direction == 3:
-        next_y += 1
-    else: # direction == 4:
-        next_x -= 1
+def move():
+    global xpos
+    global ypos
 
-    return [next_x, next_y]
+    if direction == 1:
+        ypos -= 1
+    elif direction == 2:
+        xpos += 1
+    elif direction == 3:
+        ypos += 1
+    else: # direction == 4:
+        xpos -= 1
+
 #-------------------------------------------------------------------
-def turn(direction, whichway):
+def turn(whichway):
+    global direction
+
     if whichway == 'CW':
         if direction != 4:
-            return direction + 1
+            direction += 1
         else:
-            return 1
+            direction = 1
     else:  # Turn CCW
         if direction != 1:
-            return direction - 1
+            direction -= 1
         else:
-            return 4
+            direction = 4
 
 #-------------------------------------------------------------------
-def onWhite(xpos, ypos, direction):
+def onWhite():
     grid[xpos][ypos] = 1    # Turn black
-    turn(direction)
+    turn('CW')
+    move()
 
 #-------------------------------------------------------------------
+def onBlack():
+    grid[xpos][ypos] = 0    # Turn white
+    turn('CCW')
+    move()
 
-# Initialize 100 x 100 grid
+#-------------------------------------------------------------------
+print('Initializing...')
+
+# Set playing grid size
+rows = 20000
+cols = 20000
+
+total_plays = 1000000
+
+# Initialize rows x cols grid
 # 0 = white, 1 = black
-grid = [ [0 for x in range( 10 )] for y in range( 10 ) ]
+grid = [ [0 for x in range( cols )] for y in range( rows ) ]
 
-# Initialize starting location to approx center of grid, facing 1 (of 4, e.g., North)
-xpos = 5
-ypos = 5
+# Initialize globals for starting location to approx
+# center of grid, facing 1 (of 4, e.g., North)
+xpos = cols - 50
+ypos = 50
 direction = 1
 
-for play in range(5):
-    if grid[xpos][ypos] = 0:    # On white
+minx = xpos
+miny = ypos
+maxx = xpos
+maxy = ypos
+
+cur_play = 0
+tenths = int(total_plays / 10)
+
+print('Beginning run...')
+
+for play in range(total_plays):
+    if xpos >= cols or xpos < 0 or ypos >= rows or ypos < 0:
+        break
+
+    if grid[xpos][ypos] == 0:    # On white
+        onWhite()
+    else:
+        onBlack()
+
+    if xpos > maxx:
+        maxx = xpos
+    if xpos < minx:
+        minx = xpos
+    if ypos > maxy:
+        maxy = ypos
+    if ypos < miny:
+        miny = ypos
+
+    cur_play += 1
+    if cur_play % tenths == 0:
+        pct_done = (cur_play / total_plays) * 100
+        print(int(pct_done), "%")
+
+
+
+    #print('[',xpos, ',', ypos, ']')
+    #for row in range(rows):
+    #    disp_row = str(row) + '|'
+    #    for col in range(cols):
+    #        disp_row = disp_row +' ' + str(grid[row][col])
+    #    print(disp_row)
+
+    #print ('   -------------------')
+    #print ('   0 1 2 3 4 5 6 7 8 9')
+    #print ('-----')
+
+#print(grid[3][5])
+#print(grid[4][5])
+#print(grid[4][6])
+#print(grid[3][6])
+#print(grid[2][5])
+#print(grid[9][8])
+
+total_blacks = 0
+for row in range(rows):
+    total_blacks += sum(grid[row])
+
+print('Total blacks: ', total_blacks)
+print('Ending position [', xpos, ',', ypos, ']')
+print('Total plays:', cur_play)
+print('X max and min:', maxx, minx)
+print('Y max and min:', maxy, miny)
